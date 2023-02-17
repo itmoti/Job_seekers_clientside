12ba5968a924ad494e814d12a04750b53b96967c
itmoti <motiarrahman9707@gmail.com>
2023-02-17 11:53:32 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import Router from './Routes/PublicRouter';
 
 function App() {
   return (
     <div className="App">
+   
     
-    <h1 className='text-red-50'
-    
-    >dsf</h1>
     </div>
   );
 }
